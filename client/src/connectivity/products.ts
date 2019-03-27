01ba9ca7a7f2501a3d214ef1b3252d2ef6b9d7e0
Dany <xorb@mail.com>
2019-03-27 16:10:37 -0500

    Update port

--- client/src/connectivity/products.ts
+++ client/src/connectivity/products.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URI = 'http://localhost:3000'
+const API_URI = 'http://3.17.140.168:3000'
 
 export async function getAllProducts() {
   const { data } = await axios.get(`${API_URI}/products`)
